8283b5eeb649cb87a3c644519706498c2806a5ef
Jacky Lo <jackylo.hc@gmail.com>
2023-12-18 15:47:11 +0800

    update husky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inconsolata.className}>
-        {children}
-      </body>
+      <body className={inconsolata.className}>{children}</body>
     </html>
   );
 }
